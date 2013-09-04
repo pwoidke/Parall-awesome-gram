0cea3f2ba3ee00ab9cd43f4a00dc982cfbbcc2d1
Paul W. <pwoidke@gmail.com>
2013-09-04 11:18:10 -0400

    Hover events work
    Parallelogram element transforms to a square on hover.

--- js/main.js
+++ js/main.js
@@ -4,28 +4,25 @@ $(document).ready(function(){
             new Maskew(items[i], 15, { touch: false, anchor: 'bottom', showElement: 'inline-block' });
         }
         
-    $('article').click(function(){
-        if($(this).hasClass('square'))
-        {
-            $(this).parent().transition({
-                rotate3d: '0,0,1,-15deg'
-            });
-            $(this).parent().parent().transition({
-                translate: '0px, -97px, 0px',
-                rotate3d: '0,0,1,15deg'
-            });
-            $(this).removeClass('square');
-        }
-        else
-        {
-            $(this).parent().transition({
+    $('article').hover(
+        function(){
+            $(this).parent().stop().transition({
                 rotate3d: '0,0,1,0deg'
             });
-            $(this).parent().parent().transition({
-                translate: '0px, -97px, 0px',
+            $(this).parent().parent().stop().transition({
+                translate: '0px, -97px',
                 rotate3d: '0,0,1,0deg'
             });
             $(this).addClass('square');
-        }
-    });
+        },
+        function(){
+            $(this).parent().stop().transition({
+                rotate3d: '0,0,1,-15deg'
+            });
+            $(this).parent().parent().stop().transition({
+                translate: '0px, -97px',
+                rotate3d: '0,0,1,15deg'
+            });
+            $(this).removeClass('square');
+        });
 });
