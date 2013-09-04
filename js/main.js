6e45a242aad51656181e6a61143abeadf2f969df
Paul W. <pwoidke@gmail.com>
2013-09-04 11:09:55 -0400

    Added transit.js to transform elements on click

--- js/main.js
+++ js/main.js
@@ -1,13 +1,31 @@
 $(document).ready(function(){
     var items = document.getElementsByTagName('article')
     for(i = 0, l = items.length; i < l; i++){
-            new Maskew(items[i], 12, { touch: true });
+            new Maskew(items[i], 15, { touch: false, anchor: 'bottom', showElement: 'inline-block' });
         }
         
     $('article').click(function(){
-        var $maskew = $(this).maskew(0);
-        //var html = $(this).html().trim();
-        //$(this).parent().parent().parent().parent().append(html);
-        //$(this).parent().parent().parent().remove();
+        if($(this).hasClass('square'))
+        {
+            $(this).parent().transition({
+                rotate3d: '0,0,1,-15deg'
+            });
+            $(this).parent().parent().transition({
+                translate: '0px, -97px, 0px',
+                rotate3d: '0,0,1,15deg'
+            });
+            $(this).removeClass('square');
+        }
+        else
+        {
+            $(this).parent().transition({
+                rotate3d: '0,0,1,0deg'
+            });
+            $(this).parent().parent().transition({
+                translate: '0px, -97px, 0px',
+                rotate3d: '0,0,1,0deg'
+            });
+            $(this).addClass('square');
+        }
     });
 });
