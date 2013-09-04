e811bf5cb776ef5a0dc239571a6e1294829bd1c6
Paul W. <pwoidke@gmail.com>
2013-09-04 12:37:40 -0400

    Added hover text

--- js/main.js
+++ js/main.js
@@ -1,28 +1,39 @@
+var degrees = 15;
+
 $(document).ready(function(){
     var items = document.getElementsByTagName('article')
     for(i = 0, l = items.length; i < l; i++){
-            new Maskew(items[i], 15, { touch: false, anchor: 'bottom', showElement: 'inline-block' });
-        }
-        
+        new Maskew(items[i], degrees, { touch: false, anchor: 'bottom', showElement: 'inline-block' });
+    }
+    
     $('article').hover(
         function(){
             $(this).parent().stop().transition({
                 rotate3d: '0,0,1,0deg'
             });
             $(this).parent().parent().stop().transition({
-                translate: '0px, -' + ($(this).width() * 0.24) + 'px',
+                translate: '-' + ($(this).width() / 12 ) + 'px, -' + ($(this).width() * 0.24) + 'px',
                 rotate3d: '0,0,1,0deg'
+            });
+            $(this).children('.alt').animate({
+                top: '130px'
             });
             $(this).addClass('square');
         },
         function(){
             $(this).parent().stop().transition({
-                rotate3d: '0,0,1,-15deg'
+                rotate3d: '0,0,1,-' + degrees + 'deg'
             });
             $(this).parent().parent().stop().transition({
-                translate: '0px, -' + ($(this).width() * 0.24) + 'px',
-                rotate3d: '0,0,1,15deg'
+                translate: '-' + ($(this).width() / 12 ) + 'px, -' + ($(this).width() * 0.24) + 'px',
+                rotate3d: '0,0,1,' + degrees + 'deg'
+            });
+            $(this).children('.alt').animate({
+                top: '155px'
             });
             $(this).removeClass('square');
-        });
+        }
+    );
+    
+    $('article').trigger('mouseleave'); 
 });
